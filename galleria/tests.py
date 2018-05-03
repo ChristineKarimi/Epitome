97a9ecfb29d12767375c2763b02686fa55e6f76c
christinekarimi <karimikim3@gmail.com>
2018-05-03 11:35:22 +0300

    Wrote a test for the image model to test for the save functionality

--- galleria/tests.py
+++ galleria/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Image, Category, Location
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.blackish = Image(name = 'blackish', description = 'blackish poster')
+        self.blackish.save_image()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.blackish, Image))
+
+
