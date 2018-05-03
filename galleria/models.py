a927d8661009cb8f33ecbf42f0221d675c3ab3fd
christinekarimi <karimikim3@gmail.com>
2018-05-03 11:51:27 +0300

    Added a save functionality in the models to enable saving of an image

--- galleria/models.py
+++ galleria/models.py
@@ -16,4 +16,19 @@ class Image(models.Model):
     category = models.ForeignKey('Category', on_delete = models.CASCADE, null='True', blank=True)
     location = models.ForeignKey('Location', on_delete = models.CASCADE, null='True', blank=True)
 
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()   
+
+    def delete_image(self):
+        Image.objects.filter(id = self.pk).delete() 
     
+    def update_image(self, **kwargs):
+        self.objects.filter(id = self.pk).update(**kwargs)       
+
+    @classmethod
+    def all_pics(cls):
+        pics = cls.objects.all()
+        return pics 
