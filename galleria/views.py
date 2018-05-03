43a13330f527558c06731be43dce7548cc0888d5
christinekarimi <karimikim3@gmail.com>
2018-05-03 11:27:39 +0300

    Created first view function and a template for the landing page

--- galleria/views.py
+++ galleria/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Image
 
 # Create your views here.
+def gallery(request):
+    all_pics = Image.all_pics()
+    return render(request, 'gallery.html', {"all_pics":all_pics})
