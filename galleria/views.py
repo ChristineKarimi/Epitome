da28956a312cae03d4d3ac9a6567d13a4254a46a
christinekarimi <karimikim3@gmail.com>
2018-05-04 10:14:52 +0300

    Created a search view function

--- galleria/views.py
+++ galleria/views.py
@@ -20,3 +20,15 @@ def display_images_locations(request):
 def single_pic(request, image_id):
     image = Image.get_pic(image_id)
     return render(request, 'single_pic.html', {"image":image})
+
+def search_results(request):
+    if 'image' in request.GET and request.GET['image']:
+        search_input = request.GET.get('image')
+        searched_images = Image.search_by_category(search_input)
+        message = f"{search_input}"
+
+        return render(request, 'search.html', {"message":message, "images":searched_images})
+
+    else:
+        message = "Please input something in the search field"
+        return render(request, 'search.html', {'message':message})
