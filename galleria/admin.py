88bad07217a519b35f14b415412110de63ac9591
christinekarimi <karimikim3@gmail.com>
2018-05-06 13:25:42 +0300

    Refactoring code for deployment

--- galleria/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Image, Category, Location
-
-# Register your models here.
-admin.site.register(Image)
-admin.site.register(Category)
-admin.site.register(Location)
